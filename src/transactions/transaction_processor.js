b95e82f84eb9f5ca8c3f7929d0b762ddb15fd449
Muthuraj Mohan kumar <mrohini116@gmail.com>
2022-12-19 10:35:42 +0000

    changes in sortbyAmountThenName and validateTransactions function|

--- src/transactions/transaction_processor.js
+++ src/transactions/transaction_processor.js
@@ -1,49 +1,45 @@
 function processTransactions(transActions) {
+  const txr = [];
 
-    const txr = [];
+  if (!validateTransactions(transActions)) {
+    throw new Error("Undefined collection of transactions");
+  }
 
-    if (!validateTransactions(transActions)) {
-      throw new Error("Undefined collection of transactions");
-    }
+  let txCount = {};
 
-    let txCount = {};
+  transActions.forEach((transaction) => {
+    txCount[transaction]
+      ? (txCount[transaction] += 1)
+      : (txCount[transaction] = 1);
+  });
 
-    transActions.forEach((transaction) => {
-      txCount[transaction]
-        ? (txCount[transaction] += 1)
-        : (txCount[transaction] = 1);
-    });
+  txCount = sortByAmountThenName(txCount);
 
-    txCount = sortByAmountThenName(txCount);
+  // Place them back in array for returning
+  Object.keys(txCount).forEach((key) => {
+    txr.push(`${key} ${txCount[key]}`);
+  });
 
-    // Place them back in array for returning
-    Object.keys(txCount).forEach((key, index) => {
-      txr[index] = `${key} ${txCount[key]}`;
-    });
-
-    return txr;
+  return txr;
 }
 
 function sortByAmountThenName(txCount) {
-    let sortedKeys = Object.keys(txCount).sort(function sortingFunction(itemOne, itemTwo) {
-        return  txCount[itemTwo] - txCount[itemOne] || itemOne > itemTwo || -(itemOne < itemTwo)}
-    );
+  const sortedTxCount = {};
 
-    let sortedResults = {};
-    for(let objectKey of sortedKeys) {
-        sortedResults[objectKey] = txCount[objectKey];
-    }
+  Object.keys(txCount)
+    .sort()
+    .sort((a, b) => {
+      return txCount[b] - txCount[a];
+    })
+    .forEach((key) => {
+      sortedTxCount[key] = txCount[key];
+    });
 
-    return sortedResults;
+  return sortedTxCount;
 }
 
-
 function validateTransactions(transactions) {
-    if(transactions === undefined) {
-        return false;
-    } 
-
-    return true;
+  return transactions !== undefined;
 }
 
 module.exports = processTransactions;
